ecfce4cbe88ae5508ec99cd5bec208d5a00c998e
fishku <juerui@vip.qq.com>
2019-09-08 22:34:51 +0800

    统一头部去掉(搜索，使用文档，语言切换)

--- src/components/GlobalHeader/RightContent.jsx
+++ src/components/GlobalHeader/RightContent.jsx
@@ -17,45 +17,52 @@ const GlobalHeaderRight = props => {
 
   return (
     <div className={className}>
-      <HeaderSearch
-        className={`${styles.action} ${styles.search}`}
-        placeholder={formatMessage({
-          id: 'component.globalHeader.search',
-        })}
-        dataSource={[
-          formatMessage({
-            id: 'component.globalHeader.search.example1',
-          }),
-          formatMessage({
-            id: 'component.globalHeader.search.example2',
-          }),
-          formatMessage({
-            id: 'component.globalHeader.search.example3',
-          }),
-        ]}
-        onSearch={value => {
-          console.log('input', value);
-        }}
-        onPressEnter={value => {
-          console.log('enter', value);
-        }}
-      />
-      <Tooltip
-        title={formatMessage({
-          id: 'component.globalHeader.help',
-        })}
-      >
-        <a
-          target="_blank"
-          href="https://pro.ant.design/docs/getting-started"
-          rel="noopener noreferrer"
-          className={styles.action}
+      {/* 搜索 */}
+      {/* 
+        <HeaderSearch
+          className={`${styles.action} ${styles.search}`}
+          placeholder={formatMessage({
+            id: 'component.globalHeader.search',
+          })}
+          dataSource={[
+            formatMessage({
+              id: 'component.globalHeader.search.example1',
+            }),
+            formatMessage({
+              id: 'component.globalHeader.search.example2',
+            }),
+            formatMessage({
+              id: 'component.globalHeader.search.example3',
+            }),
+          ]}
+          onSearch={value => {
+            console.log('input', value);
+          }}
+          onPressEnter={value => {
+            console.log('enter', value);
+          }}
+        /> 
+      */}
+      {/* 使用文档 */}
+      {/* 
+        <Tooltip
+          title={formatMessage({
+            id: 'component.globalHeader.help',
+          })}
         >
-          <Icon type="question-circle-o" />
-        </a>
-      </Tooltip>
+          <a
+            target="_blank"
+            href="https://pro.ant.design/docs/getting-started"
+            rel="noopener noreferrer"
+            className={styles.action}
+          >
+            <Icon type="question-circle-o" />
+          </a>
+        </Tooltip> 
+      */}
       <Avatar />
-      <SelectLang className={styles.action} />
+      {/* 语言切换 */}
+      {/* <SelectLang className={styles.action} /> */}
     </div>
   );
 };
